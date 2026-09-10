c10aa435bd7711cb7328c3c40a6fe4234283861f
Em Chanh <uyenhoang9034@gmail.com>
2026-09-10 21:50:45 +0700

    Harden voice state update event

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -1,5 +1,16 @@
 import { Events } from 'discord.js';
+
+import { logger } from '../utils/logger.js';
 import { handleMusicVoiceState } from '../services/music/musicVoiceState.js';
-export default { name: Events.VoiceStateUpdate, async execute(oldState, newState, client) {
-  await handleMusicVoiceState(client, oldState, newState);
-} };
+
+export default {
+  name: Events.VoiceStateUpdate,
+
+  async execute(oldState, newState, client) {
+    try {
+      await handleMusicVoiceState(client, oldState, newState);
+    } catch (error) {
+      logger.error('VoiceStateUpdate error:', error);
+    }
+  },
+};
