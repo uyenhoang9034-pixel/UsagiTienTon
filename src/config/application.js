dcc1e6294e6764858efc540606120c229b54795f
Em Chanh <uyenhoang9034@gmail.com>
2026-09-10 21:03:38 +0700

    Update application.js

--- src/config/application.js
+++ src/config/application.js
@@ -1,7 +1,6 @@
 import { fileURLToPath } from "url";
 import path from "path";
 import botConfig, { validateConfig } from "./bot.js";
-import { shopConfig as shop } from "./shop/index.js";
 import { pgConfig } from "./database/postgres.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
