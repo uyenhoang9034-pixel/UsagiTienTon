b39511ebbad598f00c67831e30877084b2c32de8
Em Chanh <uyenhoang9034@gmail.com>
2026-09-12 22:07:00 +0700

    feat: load Linh Mạch interaction

--- src/handlers/loaders/interactions.js
+++ src/handlers/loaders/interactions.js
@@ -23,6 +23,7 @@ const ALLOWED_INTERACTION_PATTERNS = [
   /^buttons\/tutienDailyQuest\.js$/,
   /^buttons\/tutienFormation\.js$/,
   /^buttons\/tutienShop\.js$/,
+  /^buttons\/tutienSpiritVein\.js$/,
   /^buttons\/tutienPetCodex.*\.js$/,
   /^buttons\/tutienPetCollectionReward\.js$/,
   /^buttons\/music\/.*\.js$/,
