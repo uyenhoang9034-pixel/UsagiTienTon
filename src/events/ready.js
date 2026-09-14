9b26680fb5113c7b3b889a135a690f281e0c75df
Em Chanh <uyenhoang9034@gmail.com>
2026-09-14 15:39:33 +0700

    feat: start daily world boss scheduler

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,6 +3,7 @@ import { Events } from 'discord.js';
 import { logger } from '../utils/logger.js';
 import { initRiffyAfterReady } from '../services/music/riffySetup.js';
 import { startCultivationDailyQuestScheduler } from '../services/cultivationDailyQuestScheduler.js';
+import { startCultivationWorldBossScheduler } from '../services/cultivationWorldBossScheduler.js';
 
 export default {
   name: Events.ClientReady,
@@ -27,6 +28,12 @@ export default {
       startCultivationDailyQuestScheduler(client);
     } catch (error) {
       logger.error('Could not start Cultivation Daily Quest scheduler:', error);
+    }
+
+    try {
+      startCultivationWorldBossScheduler(client);
+    } catch (error) {
+      logger.error('Could not start Cultivation World Boss scheduler:', error);
     }
 
     logger.info('UsagiTienTon ready: Music + Tiên Lộ.');
