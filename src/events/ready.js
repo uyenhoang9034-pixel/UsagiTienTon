662476aff91dfb4bd596570d8ca09443baebaf70
Em Chanh <uyenhoang9034@gmail.com>
2026-09-14 15:33:59 +0700

    feat: start automatic cultivation daily quests on ready

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,6 +2,7 @@ import { Events } from 'discord.js';
 
 import { logger } from '../utils/logger.js';
 import { initRiffyAfterReady } from '../services/music/riffySetup.js';
+import { startCultivationDailyQuestScheduler } from '../services/cultivationDailyQuestScheduler.js';
 
 export default {
   name: Events.ClientReady,
@@ -20,6 +21,12 @@ export default {
       await initRiffyAfterReady(client);
     } catch (error) {
       logger.error('Could not initialize Riffy/Lavalink after ready:', error);
+    }
+
+    try {
+      startCultivationDailyQuestScheduler(client);
+    } catch (error) {
+      logger.error('Could not start Cultivation Daily Quest scheduler:', error);
     }
 
     logger.info('UsagiTienTon ready: Music + Tiên Lộ.');
