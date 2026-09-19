8c25216688afc31364d30fb641d58c22000ecdce
Em Chanh <uyenhoang9034@gmail.com>
2026-09-19 12:33:50 +0700

    remove Music command surface: src/interactions/buttons/music/music.js

--- src/interactions/buttons/music/music.js
+++
@@ -1,9 +0,0 @@
-import {
-  getMusicButtonHandlerIds,
-  musicButtonHandler,
-} from '../../../handlers/musicButtons.js';
-
-export default getMusicButtonHandlerIds().map((name) => ({
-  name,
-  execute: musicButtonHandler.execute,
-}));
